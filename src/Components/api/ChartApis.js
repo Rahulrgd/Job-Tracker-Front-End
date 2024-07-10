5b2114b488298a463775ae0d5d889314ad58a108
RahulGupta <rahulrgd21@gmail.com>
2024-07-10 09:30:42 +0530

    Updated totalJobPostsPerDay api

--- src/Components/api/ChartApis.js
+++ src/Components/api/ChartApis.js
@@ -1,4 +1,4 @@
 import { apiClient } from "./ApiClient";
 
 export const topThreePerformers = () => apiClient.get("/v1/dashboard/top-three-performer-of-the-day-with-their-job-count");
-export const totalJobPostsPerDay = () => apiClient.get("/v1/dashboard/retrive-jobpost-count-per-day");
+export const totalJobPostsPerDay = () => apiClient.get("/v1/dashboard/retrive-jobpost-count-per-day", {params:{pageNumber:0}});
